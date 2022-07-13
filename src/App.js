de113ced282ad0cb399e83b6d4dd0018bce3a667
surendra-ksr <reddysurendra106@gmail.com>
2022-07-13 13:17:03 +0000

    https://ksr31react.ccbp.tech/

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import MatchGame from './MatchGame'
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <MatchGame />
+
+export default App
